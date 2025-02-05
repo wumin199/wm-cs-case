4455a0d1a1a272a01c9c44f5be94d797e019ae28
wumin199 <wumin199@126.com>
2025-02-05 10:45:10 +0800

    test ssh-github

--- GetStarted/Program.cs
+++ GetStarted/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Project: GetStarted");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Goodbye, World!");
