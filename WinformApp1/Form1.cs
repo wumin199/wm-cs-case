8d3f92327726667705f4cc055a920ff030ec8877
wumin199 <wumin199@126.com>
2025-02-08 12:00:33 +0800

    add newform

---
+++ WinformApp1/Form1.cs
@@ -0,0 +1,9 @@
+namespace WinformApp1;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ WinformApp1/readme.md
@@ -0,0 +1,5 @@
+mkdir WinformApp1
+cd WinformApp1
+dotnet run
+
+SSH无法跑界面
